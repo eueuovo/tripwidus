a8cdb42fa0cd46f6cbb496caee3c13ec4c9453c2
도희 <ehgml1073@gmail.com>
2026-05-23 21:22:41 +0900

    initial commit

---
+++ src/main/java/dev/dhkim/tripwidus/TripwidusApplication.java
@@ -0,0 +1,13 @@
+package dev.dhkim.tripwidus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripwidusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripwidusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dhkim/tripwidus/TripwidusApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dhkim.tripwidus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripwidusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
